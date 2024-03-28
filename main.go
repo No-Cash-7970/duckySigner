259f3a283fa0baabba1b9345ac418488ef39dae1
No-Cash-7970 <no-cash-7970@outlook.com>
2024-03-28 13:40:15 -0700

    chore: remove next.js example files

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func main() {
 	// 'BackgroundColour' is the background colour of the window.
 	// 'URL' is the URL that will be loaded into the webview.
 	app.NewWebviewWindowWithOptions(application.WebviewWindowOptions{
-		Title: "Test App",
+		Title: "Ducky Signer",
 		Mac: application.MacWindow{
 			InvisibleTitleBarHeight: 50,
 			Backdrop:                application.MacBackdropTranslucent,
