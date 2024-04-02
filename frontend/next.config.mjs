0a8c7c699b81e2dbd561d135a601fac9c02a865a
No-Cash-7970 <no-cash-7970@outlook.com>
2024-04-01 19:05:44 -0700

    chore(frontend): add config files

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   output: 'export',
   distDir: 'dist',
   reactStrictMode: false,
+  webpack: (config) => {
+    // Add use-wallet dependency modules that cause "not found" errors
+    config.externals.push('bufferutil', 'utf-8-validate', 'encoding');
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
 };
 
 export default nextConfig;
