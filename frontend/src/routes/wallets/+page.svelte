88f534b1a0fcd1f9fb401c93cc9184d7f1ea8a4a
No-Cash-7970 <no-cash-7970@outlook.com>
2024-06-11 01:43:01 -0700

    feat(frontend): add ability to list & add accounts to a wallet

--- frontend/src/routes/wallets/+page.svelte
+++ frontend/src/routes/wallets/+page.svelte
@@ -8,15 +8,29 @@
   let walletInfo: Metadata;
   // TODO: Find a more secure way to deal with the wallet password that allows the password to be temporarily stored for a short period of time
   let walletPassword = '';
-  let dialogOpen = false;
+  let passwordWrong = false;
+  let passwordCorrect = false;
+  let dialogOpen = true;
   let accounts: string[] = [];
 
   KMDService.GetWalletInfo(walletId).then(info => walletInfo = info);
   KMDService.ListAccountsInWallet(walletId).then(accts => accounts = accts);
 
-  function unlockWallet() {
-    console.log(walletPassword);
-    dialogOpen = false;
+  async function unlockWallet() {
+    try {
+      await KMDService.CheckWalletPassword(walletId, walletPassword);
+      passwordCorrect = true;
+      passwordWrong = false;
+      dialogOpen = false;
+    } catch (error) {
+      passwordWrong = true;
+      passwordCorrect = false;
+    }
+  }
+
+  async function addAccount() {
+    await KMDService.GenerateWalletAccount(walletId, walletPassword);
+    accounts = await KMDService.ListAccountsInWallet(walletId);
   }
 </script>
 
@@ -24,47 +38,49 @@
 
 {#if walletInfo}
   <h1 class="text-center text-4xl mb-8">{atob(walletInfo.Name)}</h1>
+
   <p>ID: {atob(walletInfo.ID)}</p>
 
-  <div>
-    <!-- TODO: Unlock wallet -->
-    <button type='submit' class="btn btn-primary" disabled>Unlock</button>
-    <!-- TODO: Rename wallet -->
-    <button type='submit' class="btn btn-primary" disabled>Rename</button>
-    <!-- TODO: Add account -->
-    <button type='submit' class="btn btn-primary">Add account</button>
-  </div>
-
-  {#if accounts.length > 0}
-    <ul class="menu menu-lg bg-base-200">
-      {#each accounts as account}
-        <!-- TODO: List accounts -->
-        <li><a href="#foo" class="no-underline">{'foo'}</a></li>
-      {/each}
-    </ul>
-  {:else}
-    <p class="text-center italic">No accounts</p>
+  {#if passwordCorrect}
+    <div>
+      <!-- TODO: Rename wallet -->
+      <button type='submit' class="btn btn-primary" disabled>Rename</button>
+      <button type='submit' class="btn btn-primary" on:click={addAccount}>Add account</button>
+    </div>
+    {#if accounts.length > 0}
+      <ul class="menu menu-lg bg-base-200">
+        {#each accounts as address}
+          <li><a href="/account?id={walletId}&addr={address}" class="no-underline">{address}</a></li>
+        {/each}
+      </ul>
+    {:else}
+      <p class="text-center italic">No accounts</p>
+    {/if}
   {/if}
-
-  <Dialog.Root bind:open={dialogOpen}>
-    <Dialog.Portal>
-      <Dialog.Overlay />
-      <Dialog.Content class="modal prose modal-open">
-        <div class="modal-box">
-          <Dialog.Title class="mt-0">Unlock Wallet</Dialog.Title>
-          <Dialog.Description></Dialog.Description>
-          <form id="unlock-wallet-form" on:submit|preventDefault={unlockWallet} autocomplete="off">
-            <div>
-              <label class="label" for="wallet-password-input">Wallet password</label>
-              <input type="password" bind:value={walletPassword} class="input input-bordered w-full" id="wallet-password-input" required />
-            </div>
-
-            <div class="modal-action">
-              <button type='submit' class="btn btn-primary">Unlock wallet</button>
-            </div>
-          </form>
-        </div>
-      </Dialog.Content>
-    </Dialog.Portal>
-  </Dialog.Root>
 {/if}
+
+<Dialog.Root bind:open={dialogOpen}>
+  <Dialog.Portal>
+    <Dialog.Overlay />
+    <Dialog.Content class="modal prose modal-open">
+      <div class="modal-box">
+        <Dialog.Title class="mt-0">Unlock Wallet</Dialog.Title>
+        <Dialog.Description></Dialog.Description>
+        <form id="unlock-wallet-form" on:submit|preventDefault={unlockWallet} autocomplete="off">
+          <div>
+            <label class="label" for="wallet-password-input">Wallet password</label>
+            <input type="password" bind:value={walletPassword} class="input input-bordered w-full" id="wallet-password-input" required />
+            {#if passwordWrong}
+              <div class="label bg-error px-2">
+                <span class="label-text-alt text-error-content">Incorrect password.</span>
+              </div>
+            {/if}
+          </div>
+          <div class="modal-action">
+            <button type='submit' class="btn btn-primary">Unlock wallet</button>
+          </div>
+        </form>
+      </div>
+    </Dialog.Content>
+  </Dialog.Portal>
+</Dialog.Root>
--- frontend/src/routes/wallets/page.test.ts
+++ frontend/src/routes/wallets/page.test.ts
@@ -1,27 +1,37 @@
 import {render, screen} from '@testing-library/svelte';
 import { describe, it, expect, vi } from 'vitest';
 import { writable } from 'svelte/store';
+import userEvent from '@testing-library/user-event';
 
 
 import ShowWalletPage from './+page.svelte';
+
+const walletsList = [
+  'H3PFTYORQCTLIN7PEPDCYI4ALUHNE4CE5GJIPLZA3ZBKWG23TWND4IP47A',
+  'V3NC4VRDRP33OI2R5AQXEOOXFXXRYHWDKJOCGB64C7QRCF2IWNWHPFZ4QU',
+];
 
 vi.mock('$lib/wails-bindings/duckysigner/services/kmdservice', () => ({
   GetWalletInfo: async () => ({
     ID: 'MTIz', // '123'
     Name: 'Rm9vYmFy', // 'Foobar'
   }),
-  ListAccountsInWallet: async () => [
-    'H3PFTYORQCTLIN7PEPDCYI4ALUHNE4CE5GJIPLZA3ZBKWG23TWND4IP47A',
-    'V3NC4VRDRP33OI2R5AQXEOOXFXXRYHWDKJOCGB64C7QRCF2IWNWHPFZ4QU',
-    'RMAZSNHVLAMY5AUWWTSDON4S2HIUV7AYY6MWWEMKYH63YLHAKLZNHQIL3A',
-  ],
+  ListAccountsInWallet: async () => walletsList,
+  CheckWalletPassword: async (id: string, pw: string) => {
+    if (pw !== 'badpassword') throw Error;
+  },
+  GenerateWalletAccount: async () => {
+    const newWallet = 'RMAZSNHVLAMY5AUWWTSDON4S2HIUV7AYY6MWWEMKYH63YLHAKLZNHQIL3A';
+    walletsList.push(newWallet);
+    return newWallet;
+  },
 }));
 
 vi.mock('$app/stores', () => ({
   page: writable({url: {searchParams: { get: () => '123' }}})
 }))
 
-describe('Show Wallet', () => {
+describe('Show Wallet Page', () => {
 
   it('has wallet name as heading', async () => {
 		render(ShowWalletPage);
@@ -34,8 +44,51 @@ describe('Show Wallet', () => {
     expect(screen.getByText('Back')).toBeInTheDocument();
   });
 
-  // TODO: Test for buttons
+  it('asks for wallet password', async () => {
+		render(ShowWalletPage);
 
-  // TODO: Test accounts list
+    await userEvent.click(screen.getByLabelText(/Wallet password/));
+    await userEvent.paste('badpassword');
+    await userEvent.click(screen.getByText('Unlock wallet'));
+
+    expect(screen.queryByText('Unlock Wallet')).not.toBeInTheDocument();
+  });
+
+  it('shows error message when given wrong wallet password', async () => {
+		render(ShowWalletPage);
+
+    await userEvent.click(screen.getByLabelText(/Wallet password/));
+    await userEvent.paste('goodpassword');
+    await userEvent.click(screen.getByText('Unlock wallet'));
+
+    expect(await screen.findByText(/Incorrect password/)).toBeInTheDocument();
+  });
+
+  it('can add account', async () => {
+		render(ShowWalletPage);
+
+    // Unlock wallet
+    await userEvent.click(screen.getByLabelText(/Wallet password/));
+    await userEvent.paste('badpassword');
+    await userEvent.click(screen.getByText('Unlock wallet'));
+    // Add account
+    await userEvent.click(screen.getByText(/Add account/));
+
+    expect(await screen.findByText('H3PFTYORQCTLIN7PEPDCYI4ALUHNE4CE5GJIPLZA3ZBKWG23TWND4IP47A')).toBeInTheDocument();
+    expect(await screen.findByText('V3NC4VRDRP33OI2R5AQXEOOXFXXRYHWDKJOCGB64C7QRCF2IWNWHPFZ4QU')).toBeInTheDocument();
+    expect(await screen.findByText('RMAZSNHVLAMY5AUWWTSDON4S2HIUV7AYY6MWWEMKYH63YLHAKLZNHQIL3A')).toBeInTheDocument();
+  });
+
+  it('shows accounts in wallet', async () => {
+		render(ShowWalletPage);
+
+    // Unlock wallet
+    await userEvent.click(screen.getByLabelText(/Wallet password/));
+    await userEvent.paste('badpassword');
+    await userEvent.click(screen.getByText('Unlock wallet'));
+
+    expect(await screen.findByText('H3PFTYORQCTLIN7PEPDCYI4ALUHNE4CE5GJIPLZA3ZBKWG23TWND4IP47A')).toBeInTheDocument();
+    expect(await screen.findByText('V3NC4VRDRP33OI2R5AQXEOOXFXXRYHWDKJOCGB64C7QRCF2IWNWHPFZ4QU')).toBeInTheDocument();
+  });
 
 });
