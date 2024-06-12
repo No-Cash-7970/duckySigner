45a4538382c35bcd12441cb19216672d4ee51796
No-Cash-7970 <no-cash-7970@outlook.com>
2024-06-11 23:40:00 -0700

    test(frontend): make sure onMount works properly in Vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
 		include: ['src/**/*.{test,spec}.{js,ts}'],
     environment: 'jsdom',
     setupFiles: ['./vitest-setup.ts'],
+    alias: [{ find: /^svelte$/, replacement: 'svelte/internal' }],
 	}
 });
