dbd022778ebab5970856b37b1e15ef7a2a501e9d
No-Cash-7970 <no-cash-7970@outlook.com>
2024-06-12 02:38:30 -0700

    feat(frontend): add ability to remove account from wallet

--- frontend/src/routes/account/+page.svelte
+++ frontend/src/routes/account/+page.svelte
@@ -1,16 +1,35 @@
 <script lang="ts">
+  import { goto } from '$app/navigation';
   import { page } from '$app/stores';
+  import { KMDService } from '$lib/wails-bindings/duckysigner/services';
+  import { Dialog } from "bits-ui";
   import { onMount } from 'svelte';
 
   let walletId = '';
   let acctAddr = '';
   let backLink = '/';
+  let deleteAcctDialogOpen = false;
+  let passwordWrong = false;
+  let walletPassword = '';
 
   onMount(() => {
     walletId = $page.url.searchParams.get('id') ?? '';
     acctAddr = $page.url.searchParams.get('addr') ?? '';
     backLink = walletId ? `/wallets?id=${walletId}` : '/';
   });
+
+  async function removeAcct() {
+    try {
+      await KMDService.CheckWalletPassword(walletId, walletPassword);
+      passwordWrong = false;
+      deleteAcctDialogOpen = false;
+    } catch (error) {
+      passwordWrong = true;
+    }
+
+    await KMDService.RemoveAccountFromWallet(acctAddr, walletId, walletPassword);
+    goto(backLink);
+  }
 </script>
 
 <a href="{backLink}" class="btn">Back</a>
@@ -24,8 +43,33 @@
 <div>
   <!-- TODO: See account mnemonic -->
   <button class="btn">See mnemonic</button>
-  <!-- TODO: Remove account -->
-  <button class="btn">Remove from wallet</button>
+  <button class="btn btn-error" on:click={() => deleteAcctDialogOpen = true}>Remove from wallet</button>
   <!-- TODO: Sign txn file -->
   <button class="btn">Sign transaction file</button>
 </div>
+
+<Dialog.Root bind:open={deleteAcctDialogOpen}>
+  <Dialog.Portal>
+    <Dialog.Overlay />
+    <Dialog.Content class="modal prose modal-open">
+      <div class="modal-box">
+        <Dialog.Title class="mt-0">Remove account from wallet</Dialog.Title>
+        <Dialog.Description>Enter wallet password to remove this account.</Dialog.Description>
+        <form id="unlock-wallet-form" on:submit|preventDefault={removeAcct} autocomplete="off">
+          <div>
+            <label class="label" for="wallet-password-input">Wallet password</label>
+            <input type="password" bind:value={walletPassword} class="input input-bordered w-full" id="wallet-password-input" required />
+            {#if passwordWrong}
+              <div class="label bg-error px-2">
+                <span class="label-text-alt text-error-content">Incorrect password.</span>
+              </div>
+            {/if}
+          </div>
+          <div class="modal-action">
+            <button type='submit' class="btn btn-primary">Remove this account</button>
+          </div>
+        </form>
+      </div>
+    </Dialog.Content>
+  </Dialog.Portal>
+</Dialog.Root>
--- frontend/src/routes/account/page.test.ts
+++ frontend/src/routes/account/page.test.ts
@@ -15,18 +15,37 @@ vi.mock('$app/stores', () => ({
   }}})
 }));
 
+const gotoMockFunc = vi.fn();
+vi.mock('$app/navigation', () => ({ goto: () => gotoMockFunc() }));
+
+vi.mock('$lib/wails-bindings/duckysigner/services/kmdservice', () => ({
+  RemoveAccountFromWallet: async () => { return },
+  CheckWalletPassword: async (id: string, pw: string) => {
+    if (pw !== 'badpassword') throw Error;
+  },
+}));
+
 describe('Account Information Page', () => {
 
   it('has address as heading', async () => {
 		render(AccountInfoPage);
-
-    const heading = await screen.findByRole('heading', { level: 1 });
-    expect(heading).toHaveTextContent('H3PFTYORQCTLIN7PEPDCYI4ALUHNE4CE5GJIPLZA3ZBKWG23TWND4IP47A');
+    expect(await screen.findByText('H3PFTYORQCTLIN7PEPDCYI4ALUHNE4CE5GJIPLZA3ZBKWG23TWND4IP47A')).toHaveRole('heading');
 	});
 
   it('has back button', () => {
 		render(AccountInfoPage);
     expect(screen.getByText('Back')).toBeInTheDocument();
+  });
+
+  it('can remove account from wallet', async () => {
+		render(AccountInfoPage);
+
+    await userEvent.click(screen.getByText(/Remove from wallet/));
+    await userEvent.click(screen.getByLabelText(/Wallet password/));
+    await userEvent.paste('badpassword');
+    await userEvent.click(await screen.findByText('Remove this account'));
+
+    expect(gotoMockFunc).toHaveBeenCalled();
   });
 
 });
