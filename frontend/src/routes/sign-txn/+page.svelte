c1953d1774f83b54e74215d9674ab87b3bb7158b
No-Cash-7970 <no-cash-7970@outlook.com>
2024-07-08 02:53:50 -0700

    feat(frontend): add ability to load transaction from file for signing

--- frontend/src/routes/sign-txn/+page.svelte
+++ frontend/src/routes/sign-txn/+page.svelte
@@ -2,23 +2,109 @@
   import { goto } from '$app/navigation';
   import { page } from '$app/stores';
   import { KMDService } from '$lib/wails-bindings/duckysigner/services';
+  import * as algosdk from 'algosdk';
   import { Dialog } from "bits-ui";
   import { onMount } from 'svelte';
 
   let walletId = '';
   let acctAddr = '';
   let backLink = '/';
+  let txnFileList: FileList;
+  let txnData = '';
+  let txnFileIsSigned = false;
+  let txnFileInput: HTMLInputElement;
+
+  // Adapted from https://svelte.dev/repl/file-inputs?version=4.2.18
+  $: if (txnFileList) {
+    txnData = '';
+    txnFileIsSigned = false;
+    const file = txnFileList.item(0);
+    if (file) {
+      processTxnFile(file);
+    }
+
+  }
 
   onMount(async () => {
     walletId = $page.url.searchParams.get('id') ?? '';
     acctAddr = $page.url.searchParams.get('addr') ?? '';
     backLink = (walletId && acctAddr) ? `/account?id=${walletId}&addr=${acctAddr}` : '/';
   });
+
+  /** Converts the given file's contents to bytes as a Uint8Array buffer
+   * @param file The file to convert to bytes
+   * @returns The file's contents as a Uint8Array buffer
+   */
+  async function fileToBytes(file: File) {
+    const fileData = await new Promise((resolve, reject) => {
+      const reader = Object.assign(new FileReader(), {
+        onload: () => resolve(reader.result),
+        onerror: () => reject(reader.error),
+      });
+      reader.readAsArrayBuffer(file);
+    });
+    return new Uint8Array(fileData as ArrayBuffer);
+  };
+
+  /** Processes the given file as a signed or unsigned transaction file
+   * @param file File to process
+   */
+  async function processTxnFile(file: File) {
+    const txnByteData = await fileToBytes(file);
+    let txn: algosdk.Transaction;
+
+    // Try decoding file into a `Transaction` object
+    try {
+      txn = algosdk.decodeUnsignedTransaction(txnByteData);
+    } catch (error) {
+      // Decoding the transaction as an unsigned transaction did not work, so try to decode
+      // it as a signed transaction because it may be a signed transaction
+      txn = algosdk.decodeSignedTransaction(txnByteData).txn;
+      txnFileIsSigned = true;
+    }
+
+    txnData = JSON.stringify(JSON.parse(txn.toString()), null, 2);
+  };
+
+  async function signTxn() {
+    // TODO: Sign transaction using KMD service
+  }
+
+  function cancelSignTxn() {
+    // Reset things
+    txnFileInput.value = "";
+    txnData = '';
+    txnFileIsSigned = false;
+  }
 </script>
 
 <a href="{backLink}" class="btn">Back</a>
 
 <h1 class="text-center text-4xl mb-8">Sign Transaction</h1>
 
-<p>Wallet: {walletId}</p>
-<p>Address: {acctAddr}</p>
+<div class="pb-4">
+  <p>Signing Address: {acctAddr}</p>
+
+  <div class="mt-6">
+    <label class="label" for="txn-upload-input">Choose transaction file</label>
+    <input bind:files={txnFileList} bind:this={txnFileInput} class="file-input file-input-bordered file-input-primary" type="file" name="txn-upload" id="txn-upload-input" />
+  </div>
+
+  {#if txnData}
+    <div class="grid place-content-center mb-6">
+      <p class="mb-2 text-center">Sign the following transaction?</p>
+      <div>
+        <button on:click={signTxn} class="btn btn-wide btn-secondary" type="button">Yes</button>
+        <button on:click={cancelSignTxn} class="btn btn-wide" type="button">No</button>
+      </div>
+    </div>
+
+    {#if txnFileIsSigned}
+      <div class="alert alert-warning mt-2">This transaction has already been signed.</div>
+    {/if}
+
+    <code class="card font-mono bg-neutral text-neutral-content whitespace-pre overflow-x-auto p-4">
+      {txnData}
+    </code>
+  {/if}
+</div>
--- frontend/src/routes/sign-txn/page.test.ts
+++ frontend/src/routes/sign-txn/page.test.ts
@@ -2,6 +2,7 @@ import {render, screen} from '@testing-library/svelte';
 import { describe, it, expect, vi } from 'vitest';
 import { writable } from 'svelte/store';
 import userEvent from '@testing-library/user-event';
+import * as fs from "node:fs";
 
 import SignTxnPage from './+page.svelte';
 
@@ -28,6 +29,7 @@ vi.mock('$lib/wails-bindings/duckysigner/services/kmdservice', () => ({
 vi.mock('algosdk', async (importOriginal) => {
   const actual = await importOriginal<typeof import('algosdk')>();
   return {
+    ...actual,
     microalgosToAlgos: actual.microalgosToAlgos,
     Algodv2: class {
       constructor () {
@@ -53,6 +55,58 @@ describe('Sign Transaction Page', () => {
   it('has back button', () => {
 		render(SignTxnPage);
     expect(screen.getByText('Back')).toBeInTheDocument();
+  });
+
+  it('displays transaction information of valid UNSIGNED transaction file', async () => {
+    const data = fs.readFileSync('src/testing/test_unsigned.txn.msgpack');
+    const file = new File([data], 'unsigned.txn.msgpack', { type: 'application/octet-stream' });
+
+		render(SignTxnPage);
+
+    const fileInput = screen.getByLabelText('Choose transaction file');
+    await userEvent.upload(fileInput, file);
+
+    expect(await screen.findByRole('code')).not.toBeEmptyDOMElement();
+  });
+
+  it('displays transaction information of valid SIGNED transaction file', async () => {
+    const data = fs.readFileSync('src/testing/test_signed.txn.msgpack');
+    const file = new File([data], 'signed.txn.msgpack', { type: 'application/octet-stream' });
+
+		render(SignTxnPage);
+
+    const fileInput = screen.getByLabelText('Choose transaction file');
+    await userEvent.upload(fileInput, file);
+
+    expect(await screen.findByRole('code')).not.toBeEmptyDOMElement();
+    expect(await screen.findByText('This transaction has already been signed.')).toBeInTheDocument();
+  });
+
+  it('clears file input and displayed transaction data when "No" button is clicked', async () => {
+    const data = fs.readFileSync('src/testing/test_signed.txn.msgpack');
+    const file = new File([data], 'signed.txn.msgpack', { type: 'application/octet-stream' });
+
+		render(SignTxnPage);
+
+    await userEvent.upload(screen.getByLabelText('Choose transaction file'), file);
+    await userEvent.click(await screen.findByText('No'));
+
+    // TODO: Test if file input is empty
+    expect(screen.queryByRole('code')).not.toBeInTheDocument();
+  });
+
+  it.skip('signs transaction when "Yes" button is clicked', async () => {
+    const data = fs.readFileSync('src/testing/test_signed.txn.msgpack');
+    const file = new File([data], 'signed.txn.msgpack', { type: 'application/octet-stream' });
+
+		render(SignTxnPage);
+
+    await userEvent.upload(screen.getByLabelText('Choose transaction file'), file);
+    await userEvent.click(await screen.findByText('Yes'));
+
+    // TODO: Test if file input is empty
+    expect(screen.queryByRole('code')).not.toBeInTheDocument();
+    // TODO: Test if transaction is signed and can be saved as a file
   });
 
 });
