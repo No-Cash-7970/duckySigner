054021ebfc11895e280399110817c84fcf88c86f
No-Cash-7970 <no-cash-7970@outlook.com>
2024-12-06 17:40:47 -0800

    fix(frontend): resolve 404 Not Found issue when building in non-dev mode
    The issue was caused by the odd behavior the adapter-static plugin has for +layout.server.svelte files.

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -2,5 +2,5 @@
 export const ssr = false;
 // This can be false if you're using a fallback (i.e. SPA mode)
 export const prerender = true;
-// Disable trailing slash
+// Disable trailing slash for when dev mode server is used
 export const trailingSlash = 'never';
