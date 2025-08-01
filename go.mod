6024d41c27f1043884aa50c0e4d050d4e602238d
No-Cash-7970 <no-cash-7970@outlook.com>
2025-08-01 02:40:33 -0700

    feat(session): implement storing a session to a file
    This feature is not entirely complete and the code needs some cleanup.

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/karalabe/hid v1.0.0
 	github.com/labstack/echo/v4 v4.13.4
 	github.com/labstack/gommon v0.4.2
+	github.com/marcboeker/go-duckdb v1.8.5
 	github.com/onsi/ginkgo/v2 v2.23.4
 	github.com/onsi/gomega v1.37.0
 	github.com/sirupsen/logrus v1.9.3
@@ -29,6 +30,7 @@ require (
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/ProtonMail/go-crypto v1.1.6 // indirect
 	github.com/adrg/xdg v0.5.3 // indirect
+	github.com/apache/arrow-go/v18 v18.1.0 // indirect
 	github.com/awnumar/memcall v0.2.0 // indirect
 	github.com/bep/debounce v1.2.1 // indirect
 	github.com/cloudflare/circl v1.6.0 // indirect
@@ -43,14 +45,19 @@ require (
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
+	github.com/google/flatbuffers v25.1.24+incompatible // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/pprof v0.0.0-20250403155104-27863c87afa6 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/leaanthony/go-ansi-parser v1.6.1 // indirect
 	github.com/leaanthony/u v1.1.1 // indirect
 	github.com/lmittmann/tint v1.0.7 // indirect
@@ -58,6 +65,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/petermattis/goid v0.0.0-20241025130422-66cb2e6d7274 // indirect
+	github.com/pierrec/lz4/v4 v4.1.22 // indirect
 	github.com/pjbgf/sha1cd v0.3.2 // indirect
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -72,13 +80,17 @@ require (
 	github.com/wailsapp/go-webview2 v1.0.21 // indirect
 	github.com/wailsapp/mimetype v1.4.1 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
 	go.uber.org/automaxprocs v1.6.0 // indirect
 	golang.org/x/exp v0.0.0-20250408133849-7e4ce0ab07d0 // indirect
+	golang.org/x/mod v0.25.0 // indirect
 	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
 	golang.org/x/time v0.11.0 // indirect
 	golang.org/x/tools v0.34.0 // indirect
+	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- internal/dapp_connect/session/manager.go
+++ internal/dapp_connect/session/manager.go
@@ -3,28 +3,146 @@ package session
 import (
 	"crypto/ecdh"
 	"crypto/rand"
+	"database/sql"
+	"encoding/base64"
+	"fmt"
+	"os"
+	"time"
+
+	// "database/sql"
+	_ "github.com/marcboeker/go-duckdb"
 
 	dc "duckysigner/internal/dapp_connect"
 )
 
+// TODO: Add documentation
+const (
+	DefaultConfigFile      = "session_config.json" // TODO: Encrypt this file to ensure privacy and prevent tampering
+	DefaultSessionsFile    = "sessions.parquet"
+	DefaultConfirmsFile    = "confirms.parquet"
+	DefaultDataDir         = "./dapp_connect"
+	DefaultSessionLifetime = 7 * 24 * time.Hour // 1 week
+	DefaultConfirmLifetime = 10 * time.Minute
+
+	dataDirPermissions = 0700
+)
+
+// TODO: Add documentation
+type SessionConfig struct {
+	SessionsFile        string `json:"sessions_file,omitempty"`
+	ConfirmsFile        string `json:"confirms_file,omitempty"`
+	DataDir             string `json:"data_dir,omitempty"`
+	SessionLifetimeSecs uint64 `json:"session_lifetime_secs"`
+	ConfirmLifetimeSecs uint64 `json:"confirm_lifetime_secs"`
+}
+
 // Manager is the dApp connect session manager
 type Manager struct {
-	// TODO: Complete this
+	// TODO: Add documentation
+	Curve           dc.ECDHCurve
+	SessionsFile    string
+	ConfirmsFile    string
+	DataDir         string
+	SessionLifetime time.Duration
+	ConfirmLifetime time.Duration
+}
+
+// SQL statement for created the `sessions` database table
+const sessionsCreateTblSQL = `
+CREATE TABLE sessions (
+    id VARCHAR PRIMARY KEY,
+    key VARCHAR NOT NULL,
+    expiry TIMESTAMP_S NOT NULL,
+    est TIMESTAMP_S NOT NULL,
+    dapp_id VARCHAR NOT NULL,
+    dapp_name VARCHAR,
+    dapp_url VARCHAR,
+    dapp_desc VARCHAR,
+    dapp_icon BLOB
+);
+`
+const addParquetKeySQL = "PRAGMA add_parquet_key('key', '%s');"
+const sessionsWriteToDbFileSQL = "COPY sessions TO '%s' (ENCRYPTION_CONFIG {footer_key: 'key'});"
+const sessionsSimpleInsertSQL = "INSERT INTO sessions VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+
+// NOTE: Sorting by ID reduces the files size for some reason (Maybe due to compression algorithm?)
+const sessionsParquetInsertSQL = `
+COPY (
+    (FROM read_parquet('%s', encryption_config = {footer_key: 'key'})
+    UNION
+    FROM sessions)
+)
+TO '%s' (ENCRYPTION_CONFIG {footer_key: 'key'});
+`
+
+// NewManager creates a new session manager using the given configuration for
+// creating sessions. The given ECDH curve will be used by this new session
+// manager to generate session keys.
+func NewManager(curve dc.ECDHCurve, sessionConfig *SessionConfig) *Manager {
+	var (
+		sessionLife  time.Duration
+		confirmLife  time.Duration
+		dataDir      string
+		sessionsFile string
+		confirmsFile string
+	)
+
+	// Use default config if no config was given
+	if sessionConfig == nil {
+		sessionLife = DefaultSessionLifetime
+		confirmLife = DefaultConfirmLifetime
+		dataDir = DefaultDataDir
+		sessionsFile = DefaultSessionsFile
+		confirmsFile = DefaultConfirmsFile
+	} else {
+		sessionLife = time.Duration(sessionConfig.SessionLifetimeSecs) * time.Second
+		confirmLife = time.Duration(sessionConfig.ConfirmLifetimeSecs) * time.Second
+		dataDir = sessionConfig.DataDir
+		sessionsFile = sessionConfig.SessionsFile
+		confirmsFile = sessionConfig.ConfirmsFile
+
+		// If no session lifetime was given
+		if sessionsFile == "" {
+			sessionLife = DefaultSessionLifetime
+		}
+		// If no confirmation lifetime was given
+		if confirmsFile == "" {
+			confirmLife = DefaultConfirmLifetime
+		}
+
+		// If no file name for the sessions database file was given
+		if sessionsFile == "" {
+			sessionsFile = DefaultSessionsFile
+		}
+		// If no file name for the confirmations database file was given
+		if confirmsFile == "" {
+			confirmsFile = DefaultConfirmsFile
+		}
+	}
+
+	return &Manager{
+		Curve:           curve,
+		SessionLifetime: sessionLife,
+		ConfirmLifetime: confirmLife,
+		DataDir:         dataDir,
+		SessionsFile:    sessionsFile,
+		ConfirmsFile:    confirmsFile,
+	}
 }
 
 // GenerateSession creates a new session by generating a new session key pair
 // for the dApp with the given ID with the given dApp data
-func (sm *Manager) GenerateSession(dappId *ecdh.PublicKey, dappData *dc.DappData, curve dc.ECDHCurve) (session *Session, err error) {
+func (sm *Manager) GenerateSession(dappId *ecdh.PublicKey, dappData *dc.DappData) (session *Session, err error) {
 	// Generate session key pair
-	sessionKey, err := curve.GenerateKey(rand.Reader)
+	sessionKey, err := sm.Curve.GenerateKey(rand.Reader)
 	if err != nil {
 		return
 	}
 
-	// TODO: Complete this
+	exp := time.Now().Add(sm.SessionLifetime)
 	session = &Session{
-		key: sessionKey,
-		// exp: time.Now().Add(time.Duration(service.Config.SessionLifetimeSecs) * time.Second),
+		key:      sessionKey,
+		exp:      exp,
 		dappId:   dappId,
 		dappData: dappData,
 	}
@@ -44,10 +162,103 @@ func (sm *Manager) GetAllSessions() ([]*Session, error) {
 	return []*Session{}, nil
 }
 
-// StoreSession attempts to store the given session
-func (sm *Manager) StoreSession(session *Session) error {
-	// TODO: Complete this
-	return nil
+// StoreSession attempts to store the given session using the given file
+// encryption key to access the sessions database file
+func (sm *Manager) StoreSession(session *Session, fileEncKey []byte) (err error) {
+	// Ensure data directory exists
+	err = os.Mkdir(sm.DataDir, dataDirPermissions)
+	if err != nil && !os.IsExist(err) {
+		return
+	}
+
+	sessionsFilePath := sm.DataDir + "/" + sm.SessionsFile
+
+	// Check if session file exists
+	_, err = os.Stat(sessionsFilePath)
+	if err != nil && !os.IsNotExist(err) {
+		// Could not access file some reason other than that it does not exist
+		// (e.g. permissions, drive failure)
+		return
+	}
+
+	sessionsFileExists := !os.IsNotExist(err)
+
+	// Open DuckDB in in-memory mode
+	db, err := sql.Open("duckdb", "")
+	if err != nil {
+		return
+	}
+	defer db.Close()
+
+	// Add key for decrypting and encrypting file
+	// NOTE: This PRAGMA statement does not work as a prepared statement, but
+	// there is no risk of SQL injection
+	_, err = db.Exec(fmt.Sprintf(addParquetKeySQL, base64.StdEncoding.EncodeToString(fileEncKey)))
+	if err != nil {
+		return
+	}
+
+	// Convert some of the session data for storage
+	b64Encoder := base64.StdEncoding
+	sessionKey := session.Key()
+	sessionIdB64 := b64Encoder.EncodeToString(sessionKey.PublicKey().Bytes())
+	sessionKeyB64 := b64Encoder.EncodeToString(sessionKey.Bytes())
+	dappIdB64 := b64Encoder.EncodeToString(session.DappId().Bytes())
+	// Ensure dApp data is not nil
+	var dappData *dc.DappData
+	if session.dappData == nil {
+		dappData = &dc.DappData{}
+	} else {
+		dappData = session.dappData
+	}
+
+	// Create new table
+	_, err = db.Exec(sessionsCreateTblSQL)
+	if err != nil {
+		return
+	}
+
+	dappIconB64, decodeErr := b64Encoder.DecodeString(dappData.Icon)
+	if decodeErr != nil {
+		return decodeErr
+	}
+
+	// Insert session
+	_, err = db.Exec(sessionsSimpleInsertSQL,
+		sessionIdB64,
+		sessionKeyB64,
+		// DuckDB uses ISO8601 format for timestamps
+		// Source: <https://duckdb.org/docs/stable/sql/data_types/timestamp>
+		session.Expiration().UTC().Format(time.DateTime),
+		session.EstablishedAt().UTC().Format(time.DateTime),
+		dappIdB64,
+		dappData.Name,
+		dappData.URL,
+		dappData.Description,
+		dappIconB64,
+	)
+	if err != nil {
+		return
+	}
+
+	// Write to file
+	if sessionsFileExists {
+		// TODO: Check if session is already stored
+
+		// Writing a new session into an existing Parquet file is a little more
+		// complex than writing a session to a new Parquet file
+		_, err = db.Exec(fmt.Sprintf(sessionsParquetInsertSQL, sessionsFilePath, sessionsFilePath))
+		if err != nil {
+			return
+		}
+	} else {
+		_, err = db.Exec(fmt.Sprintf(sessionsWriteToDbFileSQL, sessionsFilePath))
+		if err != nil {
+			return
+		}
+	}
+
+	return
 }
 
 // RemoveSession attempts to remove the stored session with the given ID
