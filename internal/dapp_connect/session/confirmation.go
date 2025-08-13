9016068adced8a8fdcb2736b672bdf7d9d36bc11
No-Cash-7970 <no-cash-7970@outlook.com>
2025-08-13 03:54:08 -0700

    refactor(session): combine `Confirmation` & `ConfirmationToken` into one struct

--- internal/dapp_connect/session/confirmation.go
+++ internal/dapp_connect/session/confirmation.go
@@ -2,38 +2,210 @@ package session
 
 import (
 	"crypto/ecdh"
+	"encoding/base64"
+	"errors"
+	"time"
+
+	"aidanwoods.dev/go-paseto"
+
+	dc "duckysigner/internal/dapp_connect"
 )
 
-// Confirmation contains data about a dApp connect session "confirmation". A
-// "confirmation" is created when a dApp initializes a session and is no longer
-// needed when the session is confirmed.
+const (
+	// DappIdClaimName is the name for the "claim" that contains the dApp ID
+	// within a PASETO for a confirmation token
+	DappIdClaimName = "dapp"
+	// SessionKeyClaimName is the name for the "claim" that contains the session
+	// key within a PASETO for a confirmation token
+	SessionKeyClaimName = "skey"
+	// ConfirmCodeClaimName is the name for the "claim" that contains the
+	// confirmation code within a PASETO for a confirmation token
+	ConfirmCodeClaimName = "code"
+
+	// MissingConfirmTokenDappIdErrMsg is the error message for when the dApp ID
+	// is missing within the confirmation token
+	MissingConfirmTokenDappIdErrMsg = "missing dApp ID in confirmation token"
+	// MissingConfirmTokenSessionKeyErrMsg is the error message for when the
+	// session key is missing within the confirmation token
+	MissingConfirmTokenSessionKeyErrMsg = "missing session key in confirmation token"
+	// MissingConfirmTokenConfirmKeyErrMsg is the error message for when the
+	// confirmation key is missing within the confirmation token
+	MissingConfirmTokenConfirmKeyErrMsg = "missing confirmation key in confirmation token"
+	// MissingConfirmTokenCodeErrMsg is the error message for when the
+	// confirmation code is missing within the confirmation token
+	MissingConfirmTokenCodeErrMsg = "missing confirmation code in confirmation token"
+)
+
+// Confirmation contains the data about a session confirmation and the data
+// needed to create a confirmation token. The confirmation token is used to
+// "confirm" an initialized dApp connect session.
 // NOTE: None of the struct fields are exported to make them read-only after the
 // struct is created
 type Confirmation struct {
-	// Confirmation secret key
-	key *ecdh.PrivateKey
+	key        *ecdh.PrivateKey
+	code       string
+	exp        time.Time
+	dappId     *ecdh.PublicKey
+	sessionKey *ecdh.PrivateKey
+	// TODO: issuedAt date?
 }
 
-// NewConfirmation creates a new Confirmation using the given confirmation
-// secret key
-func NewConfirmation(key *ecdh.PrivateKey) Confirmation {
-	return Confirmation{
-		key: key,
+// NewConfirmation creates a new Confirmation with the given dApp ID, session
+// secret key, confirmation secret key, confirmation code and confirmation
+// expiration date-time
+func NewConfirmation(
+	dappId *ecdh.PublicKey,
+	sessionKey *ecdh.PrivateKey,
+	confirmKey *ecdh.PrivateKey,
+	confirmCode string,
+	confirmExp time.Time,
+) *Confirmation {
+	return &Confirmation{
+		dappId:     dappId,
+		sessionKey: sessionKey,
+		key:        confirmKey,
+		code:       confirmCode,
+		exp:        confirmExp,
 	}
 }
 
-// ID returns the confirmation ID
+// ID returns the confirmation ID that uniquely identifies an unconfirmed
+// initialized session
 func (confirm *Confirmation) ID() *ecdh.PublicKey {
 	return confirm.key.PublicKey()
 }
 
-// Key returns the confirmation secret key
+// Key returns the confirmation key that will be used to create the confirmation
+// token
 func (confirm *Confirmation) Key() *ecdh.PrivateKey {
 	return confirm.key
 }
 
-// SharedKey returns the confirmation shared secret key that is derived from
+// Code returns the confirmation code, which is used when confirming an
+// initialized session
+func (confirm *Confirmation) Code() string {
+	return confirm.code
+}
+
+// DappId returns the ID of the dApp the confirmation is for
+func (confirm *Confirmation) DappId() *ecdh.PublicKey {
+	return confirm.dappId
+}
+
+// SessionKey returns the session key for the session to be confirmed. The
+// session key is included within the confirmation token to prevent the token
+// from being used to confirm an initialized session more than once.
+func (confirm *Confirmation) SessionKey() *ecdh.PrivateKey {
+	return confirm.sessionKey
+}
+
+// Expiration returns the date-time the confirmation expires and cannot be used
+func (confirm *Confirmation) Expiration() time.Time {
+	return confirm.exp
+}
+
+// SharedKey returns the confirmation *shared* secret key that is derived from
 // confirmation secret key and the given dApp ID
-func (confirm *Confirmation) SharedKey(dappId *ecdh.PublicKey) ([]byte, error) {
-	return confirm.key.ECDH(dappId)
+func (confirm *Confirmation) SharedKey() ([]byte, error) {
+	return confirm.key.ECDH(confirm.dappId)
+}
+
+// GenerateTokenString uses the confirmation token data to create an encrypted
+// confirmation token string. This string is given to the dApp for it to confirm
+// its initialized session.
+func (confirm *Confirmation) GenerateTokenString() (string, error) {
+	if confirm.key == nil {
+		return "", errors.New(MissingConfirmTokenConfirmKeyErrMsg)
+	}
+
+	if confirm.dappId == nil {
+		return "", errors.New(MissingConfirmTokenDappIdErrMsg)
+	}
+
+	if confirm.code == "" {
+		return "", errors.New(MissingConfirmTokenCodeErrMsg)
+	}
+
+	if confirm.sessionKey == nil {
+		return "", errors.New(MissingConfirmTokenSessionKeyErrMsg)
+	}
+
+	// NOTE: Refer to the following links for explanations of "claims", a term
+	// that PASETO borrowed from JWT.
+	// - <https://github.com/paseto-standard/paseto-spec/blob/master/docs/02-Implementation-Guide/04-Claims.md>
+	// - <https://medium.com/@dmosyan/json-web-token-claims-explained-e78a708ec43c>
+
+	// Add "claims" to be included and encrypted in the PASETO
+	confirmPaseto := paseto.NewToken()
+	confirmPaseto.SetExpiration(confirm.exp)
+	confirmPaseto.SetString(DappIdClaimName, base64.StdEncoding.EncodeToString(confirm.dappId.Bytes()))
+	confirmPaseto.SetString(ConfirmCodeClaimName, confirm.code)
+	confirmPaseto.Set(SessionKeyClaimName, base64.StdEncoding.EncodeToString(confirm.sessionKey.Bytes()))
+	// Use confirmation key to encrypt the PASETO
+	pasetoKey, err := paseto.V4SymmetricKeyFromBytes(confirm.key.Bytes())
+	if err != nil {
+		return "", err
+	}
+
+	return confirmPaseto.V4Encrypt(pasetoKey, nil), nil
+}
+
+// DecryptToken attempts to decrypt the given confirmation token using the given
+// confirmation key and extracts the contents of the token into a Confirmation.
+// The given curve is used to parse ECDH public and private key strings within
+// the token. The same curve used to generate the ECDH keys must be used here.
+func DecryptToken(confirmToken string, confirmKey *ecdh.PrivateKey, curve dc.ECDHCurve) (*Confirmation, error) {
+	pasetoKey, err := paseto.V4SymmetricKeyFromBytes(confirmKey.Bytes())
+	if err != nil {
+		return nil, err
+	}
+
+	// Decrypt PASETO
+	parser := paseto.NewParserWithoutExpiryCheck() // Expiry check is expected to be done elsewhere
+	parsedToken, err := parser.ParseV4Local(pasetoKey, confirmToken, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract dApp ID
+	dappIdB64, err := parsedToken.GetString(DappIdClaimName)
+	if err != nil {
+		return nil, err
+	}
+	dappIdBytes, err := base64.StdEncoding.DecodeString(dappIdB64)
+	if err != nil {
+		return nil, err
+	}
+	dappId, err := curve.NewPublicKey(dappIdBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract session key
+	sessionKeyB64, err := parsedToken.GetString(SessionKeyClaimName)
+	if err != nil {
+		return nil, err
+	}
+	sessionKeyBytes, err := base64.StdEncoding.DecodeString(sessionKeyB64)
+	if err != nil {
+		return nil, err
+	}
+	sessionKey, err := curve.NewPrivateKey(sessionKeyBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract confirmation code
+	code, err := parsedToken.GetString(ConfirmCodeClaimName)
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract expiration
+	exp, err := parsedToken.GetExpiration()
+	if err != nil {
+		return nil, err
+	}
+
+	return NewConfirmation(dappId, sessionKey, confirmKey, code, exp), nil
 }
--- internal/dapp_connect/session/confirmation_test.go
+++ internal/dapp_connect/session/confirmation_test.go
@@ -3,7 +3,10 @@ package session_test
 import (
 	"crypto/ecdh"
 	"crypto/rand"
+	"encoding/base64"
+	"time"
 
+	"aidanwoods.dev/go-paseto"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
@@ -12,6 +15,33 @@ import (
 
 var _ = Describe("DApp Connect Confirmation", func() {
 
+	Describe("DappId()", func() {
+		It("returns the dApp ID", func() {
+			By("Generating a dApp key pair (dApp ID & key)")
+			dappKey, err := ecdh.X25519().GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			dappId := dappKey.PublicKey()
+
+			By("Creating a confirmation")
+			confirmToken := session.NewConfirmation(dappId, nil, nil, "", time.Time{})
+
+			Expect(confirmToken.DappId()).To(Equal(dappId))
+		})
+	})
+
+	Describe("SessionKey()", func() {
+		It("returns the session secret key", func() {
+			By("Generating a session key pair (session ID & key)")
+			sessionKey, err := ecdh.X25519().GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+
+			By("Creating a confirmation")
+			confirmToken := session.NewConfirmation(nil, sessionKey, nil, "", time.Time{})
+
+			Expect(confirmToken.SessionKey()).To(Equal(sessionKey))
+		})
+	})
+
 	Describe("ID()", func() {
 		It("returns the confirmation ID", func() {
 			By("Generating a confirmation key pair (confirmation ID & key)")
@@ -19,10 +49,10 @@ var _ = Describe("DApp Connect Confirmation", func() {
 			Expect(err).ToNot(HaveOccurred())
 			confirmId := confirmKey.PublicKey()
 
-			By("Creating a confirmation using generated confirmation key pair")
-			confirm := session.NewConfirmation(confirmKey)
+			By("Creating a confirmation")
+			confirmToken := session.NewConfirmation(nil, nil, confirmKey, "", time.Time{})
 
-			Expect(confirm.ID()).To(Equal(confirmId))
+			Expect(confirmToken.ID()).To(Equal(confirmId))
 		})
 	})
 
@@ -32,10 +62,26 @@ var _ = Describe("DApp Connect Confirmation", func() {
 			confirmKey, err := ecdh.X25519().GenerateKey(rand.Reader)
 			Expect(err).ToNot(HaveOccurred())
 
-			By("Creating a confirmation using confirmation key")
-			confirm := session.NewConfirmation(confirmKey)
+			By("Creating a confirmation")
+			confirmToken := session.NewConfirmation(nil, nil, confirmKey, "", time.Time{})
 
-			Expect(confirm.Key()).To(Equal(confirmKey))
+			Expect(confirmToken.Key()).To(Equal(confirmKey))
+		})
+	})
+
+	Describe("Code()", func() {
+		It("returns the confirmation ID", func() {
+			confirmCode := "123456"
+			confirmToken := session.NewConfirmation(nil, nil, nil, confirmCode, time.Time{})
+			Expect(confirmToken.Code()).To(Equal(confirmCode))
+		})
+	})
+
+	Describe("Expiration()", func() {
+		It("returns the expiration date-time", func() {
+			testTime := time.Now()
+			confirmToken := session.NewConfirmation(nil, nil, nil, "", testTime)
+			Expect(confirmToken.Expiration()).To(Equal(testTime))
 		})
 	})
 
@@ -52,13 +98,189 @@ var _ = Describe("DApp Connect Confirmation", func() {
 			dappId := dappKey.PublicKey()
 
 			By("Creating a confirmation using generated confirmation key pair and dApp ID")
-			confirm := session.NewConfirmation(confirmKey)
+			confirm := session.NewConfirmation(dappId, nil, confirmKey, "", time.Time{})
 
 			By("Deriving shared key using dApp key and confirmation ID")
 			sharedKey, err := dappKey.ECDH(confirmId)
 			Expect(err).ToNot(HaveOccurred())
 
-			Expect(confirm.SharedKey(dappId)).To(Equal(sharedKey))
+			Expect(confirm.SharedKey()).To(Equal(sharedKey))
+		})
+	})
+
+	Describe("GenerateTokenString()", func() {
+		It("returns token string", func() {
+			curve := ecdh.X25519()
+			// Create dApp ID
+			dappKey, err := curve.GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			dappId := dappKey.PublicKey()
+			// Create session key
+			sessionKey, err := curve.GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			// Create confirmation key
+			confirmKey, err := curve.GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			// Create other confirmation token data
+			confirmCode := "123456"
+			exp := time.Now().Add(5 * time.Minute)
+
+			By("Creating an encrypted confirmation token string")
+			confirmToken := session.NewConfirmation(dappId, sessionKey, confirmKey, confirmCode, exp)
+			confirmTokenString, err := confirmToken.GenerateTokenString()
+			Expect(err).ToNot(HaveOccurred())
+
+			By("Decrypting token string")
+			pasetoKey, err := paseto.V4SymmetricKeyFromBytes(confirmKey.Bytes())
+			Expect(err).ToNot(HaveOccurred())
+			parser := paseto.NewParser()
+			parsedToken, err := parser.ParseV4Local(pasetoKey, confirmTokenString, nil)
+			Expect(err).ToNot(HaveOccurred())
+
+			By("Checking 'claims' in decrypted token")
+			// Expiration
+			parsedExp, err := parsedToken.GetExpiration()
+			Expect(err).ToNot(HaveOccurred())
+			Expect(parsedExp).To(BeTemporally("~", exp, time.Second), "Decrypted token contains expiry")
+			// DApp ID
+			parsedDappId, err := parsedToken.GetString("dapp")
+			Expect(err).ToNot(HaveOccurred())
+			dappIdB64 := base64.StdEncoding.EncodeToString(dappId.Bytes())
+			Expect(parsedDappId).To(Equal(dappIdB64), "Decrypted token contains dApp ID")
+			// Confirmation code
+			parsedCode, err := parsedToken.GetString("code")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(parsedCode).To(Equal(confirmCode), "Decrypted token contains confirmation code")
+			// Session key
+			parsedSKey, err := parsedToken.GetString("skey")
+			Expect(err).ToNot(HaveOccurred())
+			sessionKeyB64 := base64.StdEncoding.EncodeToString(sessionKey.Bytes())
+			Expect(parsedSKey).To(Equal(sessionKeyB64), "Decrypted token contains session key")
+		})
+
+		It("fails when dApp ID is missing", func() {
+			curve := ecdh.X25519()
+			// Create session key
+			sessionKey, err := curve.GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			// Create confirmation key
+			confirmKey, err := curve.GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			// Create other confirmation token data
+			confirmCode := "123456"
+			exp := time.Now().Add(5 * time.Minute)
+
+			By("Attempting to create an encrypted confirmation token string without a dApp ID")
+			confirmToken := session.NewConfirmation(nil, sessionKey, confirmKey, confirmCode, exp)
+			_, err = confirmToken.GenerateTokenString()
+			Expect(err).To(MatchError(session.MissingConfirmTokenDappIdErrMsg))
+		})
+
+		It("fails when session key is missing", func() {
+			curve := ecdh.X25519()
+			// Create dApp ID
+			dappKey, err := curve.GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			dappId := dappKey.PublicKey()
+			// Create confirmation key
+			confirmKey, err := curve.GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			// Create other confirmation token data
+			confirmCode := "123456"
+			exp := time.Now().Add(5 * time.Minute)
+
+			By("Attempting to create an encrypted confirmation token string without a session key")
+			confirmToken := session.NewConfirmation(dappId, nil, confirmKey, confirmCode, exp)
+			_, err = confirmToken.GenerateTokenString()
+			Expect(err).To(MatchError(session.MissingConfirmTokenSessionKeyErrMsg))
+		})
+
+		It("fails when confirmation code is missing", func() {
+			curve := ecdh.X25519()
+			// Create dApp ID
+			dappKey, err := curve.GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			dappId := dappKey.PublicKey()
+			// Create session key
+			sessionKey, err := curve.GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			// Create confirmation key
+			confirmKey, err := curve.GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			// Create other confirmation token data
+			confirmCode := ""
+			exp := time.Now().Add(5 * time.Minute)
+
+			By("Attempting to create an encrypted confirmation token string without a confirmation code")
+			confirmToken := session.NewConfirmation(dappId, sessionKey, confirmKey, confirmCode, exp)
+			_, err = confirmToken.GenerateTokenString()
+			Expect(err).To(MatchError(session.MissingConfirmTokenCodeErrMsg))
+		})
+
+		It("fails when confirmation key is missing", func() {
+			curve := ecdh.X25519()
+			// Create dApp ID
+			dappKey, err := curve.GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			dappId := dappKey.PublicKey()
+			// Create session key
+			sessionKey, err := curve.GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			// Create other confirmation token data
+			confirmCode := "123456"
+			exp := time.Now().Add(5 * time.Minute)
+
+			By("Attempting to create an encrypted confirmation token string without a confirmation key")
+			confirmToken := session.NewConfirmation(dappId, sessionKey, nil, confirmCode, exp)
+			_, err = confirmToken.GenerateTokenString()
+			Expect(err).To(MatchError(session.MissingConfirmTokenConfirmKeyErrMsg))
+		})
+	})
+
+	Describe("DecryptToken()", func() {
+		It("decrypts given confirmation token string", func() {
+			curve := ecdh.X25519()
+			// Create dApp ID
+			dappKey, err := curve.GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			dappId := dappKey.PublicKey()
+			// Create session key
+			sessionKey, err := curve.GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			// Create confirmation key
+			confirmKey, err := curve.GenerateKey(rand.Reader)
+			Expect(err).ToNot(HaveOccurred())
+			// Create other confirmation token data
+			confirmCode := "123456"
+			exp := time.Now().Add(5 * time.Minute)
+
+			By("Creating a confirmation encrypted token string")
+			token := paseto.NewToken()
+			token.SetExpiration(exp)
+			token.SetString(session.DappIdClaimName, base64.StdEncoding.EncodeToString(dappId.Bytes()))
+			token.SetString(session.ConfirmCodeClaimName, confirmCode)
+			token.SetString(session.SessionKeyClaimName, base64.StdEncoding.EncodeToString(sessionKey.Bytes()))
+			pasetoKey, err := paseto.V4SymmetricKeyFromBytes(confirmKey.Bytes())
+			Expect(err).ToNot(HaveOccurred())
+			encryptedTokenString := token.V4Encrypt(pasetoKey, nil)
+
+			By("Decrypting the encrypted confirmation token")
+			decryptedToken, err := session.DecryptToken(encryptedTokenString, confirmKey, curve)
+			Expect(err).ToNot(HaveOccurred())
+
+			By("Checking 'claims' in decrypted token")
+			// Expiration
+			tokenExp := decryptedToken.Expiration()
+			Expect(tokenExp).To(BeTemporally("~", exp, time.Second), "Decrypted token contains expiry")
+			// DApp ID
+			tokenDappId := decryptedToken.DappId()
+			Expect(tokenDappId).To(Equal(dappId), "Decrypted token contains dApp ID")
+			// Confirmation code
+			tokenCode := decryptedToken.Code()
+			Expect(tokenCode).To(Equal(confirmCode), "Decrypted token contains confirmation code")
+			// Session key
+			tokenSKey := decryptedToken.SessionKey()
+			Expect(tokenSKey).To(Equal(sessionKey), "Decrypted token contains session key")
 		})
 	})
 })
