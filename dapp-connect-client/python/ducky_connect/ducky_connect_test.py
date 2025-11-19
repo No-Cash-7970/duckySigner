c304255df4861d37d7980405645ee6dbdc911bcb
No-Cash-7970 <no-cash-7970@outlook.com>
2025-11-19 01:50:55 -0800

    test(connect_client_py): skip all tests
    There is nothing to test

--- dapp-connect-client/python/ducky_connect/ducky_connect_test.py
+++ dapp-connect-client/python/ducky_connect/ducky_connect_test.py
@@ -26,10 +26,12 @@ class TestDCEstablishSession:
 class TestDCRetrieveSession:
     """DappConnect.retrieve_session()"""
 
+    @m.skip
     def test_data_exists(self):
         """Returns stored session data, if it exists"""
         pass
 
+    @m.skip
     def test_data_not_exist(self):
         """Returns null if there is no stored session data"""
         pass
@@ -37,10 +39,12 @@ class TestDCRetrieveSession:
 class TestDCEndSession:
     """DappConnect.end_session()"""
 
+    @m.skip
     def test_server_contact_success(self):
         """Removes stored session after successfully contacting server"""
         pass
 
+    @m.skip
     def test_server_contact_fail(self):
         """Still removes stored session after contacting server fails"""
         pass
@@ -48,18 +52,22 @@ class TestDCEndSession:
 class TestDCSignTransaction:
     """DappConnect.sign_transaction()"""
 
+    @m.skip
     def test_no_signer(self):
         """Signs transaction WITHOUT signer address given"""
         pass
 
+    @m.skip
     def test_with_signer(self):
         """Signs transaction WITH signer address given"""
         pass
 
+    @m.skip
     def test_signing_fail(self):
         """Throws error if transaction signing fails"""
         pass
 
+    @m.skip
     def test_no_session(self):
         """Throws error if no session has been established"""
         pass
